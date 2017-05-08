f5897848042472fe97b457670ff6254f9fbce911
Giovanni vargas-Limon <Obliviousair22@gmail.com>
2017-05-08 15:01:08 -0700

    dsafa

--- final_project.js
+++ final_project.js
@@ -19,10 +19,12 @@ var four = document.querySelector("input[name='mode']checked").value
 if(four=="80% cdr"){
   quizzScore = quizzScore+1
 }
-var five =document.querySelector("input[name='company']checked").value
+var five = document.querySelector("input[name='company']checked").value
 if(five=="Riot Games"){
   quizzScore = quizzScore+1
 }
+alert("Your score is")
+alert(quizzScore)
 
 }
 // DO NOT EDIT CODE BELOW THIS LINE!
